f4b323aab49b8ae8f3fd1247b66243b3f5d35061
Myiah Moore <myiahsade@gmail.com>
2018-07-24 10:27:35 -0500

    check in

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int number1 = 23;
+        int number2 = 45;
+        System.out.println("The largest number is:"  + largestNumber(number1, number2));
+    }
+    public static int largestNumber(int n1 , int n2)
+    {
+        return  n2;
     }
 }
+
